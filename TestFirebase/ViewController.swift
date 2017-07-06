f396e40f45406b9d3655b9d4c26ad364fd5d5f50
sajjan_giri <sajjangiri@gmail.com>
2017-07-06 12:12:08 +0545

    Initial Commit

---
+++ TestFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFirebase
+//
+//  Created by Eeposit 01 on 7/6/17.
+//  Copyright © 2017 Eeposit 01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
